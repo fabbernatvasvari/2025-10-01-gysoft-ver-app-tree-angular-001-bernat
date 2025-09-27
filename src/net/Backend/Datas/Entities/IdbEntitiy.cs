6017ccdd8835a1bdebf41227bee8e41216044301
gylm9 <gylm9@LACI>
2025-09-27 17:27:34 +0200

    függőségek beállítása

---
+++ src/net/Backend/Datas/Entities/IdbEntitiy.cs
@@ -0,0 +1,9 @@
+﻿namespace MyApp.Backend.Datas.Entities
+{
+    public interface IdbEntitiy<TEntity> where TEntity : class, new()
+    {
+        public string GetDbSetName() => new TEntity().GetType().Name;
+        public Guid Id { get; set; }
+        public bool HasId => Id != Guid.Empty ? true : false;
+        }
+}
