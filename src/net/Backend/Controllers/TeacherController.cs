9d3616125494fc8d9a40d74d2045906e5a737bd1
Csaba Gyuris <gyuris.csaba@vasvari.org>
2025-09-28 12:57:02 +0200

    Teszteléshez: /api/Teacher/count mindig a "10" értéket szolgáltatja

---
+++ src/net/Backend/Controllers/TeacherController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeacherController : ControllerBase
+    {        
+        public TeacherController()
+        {            
+        }
+        
+        [HttpGet("count")]
+        public IActionResult GetNumberOfTeacher()
+        {
+            return Ok(10);
+        }
+    }
+}
