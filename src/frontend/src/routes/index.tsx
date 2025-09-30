f734955a5d196cc0614873dcb9bd3fc2689cdb32
jaydendeveloper <contact.jaydendev@gmail.com>
2025-09-30 10:13:19 +0200

    Data fetching logic implemented

--- src/frontend/src/routes/index.tsx
+++ src/frontend/src/routes/index.tsx
@@ -1,13 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { getTeacherCount } from "../helpers/apiHelpers";
 
 export const Route = createFileRoute("/")({
 	component: Index,
 });
 
 function Index() {
+	const { isPending, isError, data, error } = useQuery({
+		queryKey: ["TeacherCount"],
+		queryFn: getTeacherCount,
+	});
+
+	if (isPending) return <div>Loading...</div>;
+	if (isError) return <div>Error: {JSON.stringify(error)}</div>;
+
 	return (
 		<div className="p-2">
-			<h3>Welcome Home! {import.meta.env.VITE_BACKEND_URL}</h3>
+			<h3>Teacher count: {data?.count}</h3>
 		</div>
 	);
 }
