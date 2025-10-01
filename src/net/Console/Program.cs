d65c70571e0ccd533afaf6b79c2b274efeaa27fb
jaydendeveloper <contact.jaydendev@gmail.com>
2025-10-01 09:28:19 +0200

    Data fetching from API implemented

--- src/net/Console/Program.cs
+++ src/net/Console/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MyApp.Console.Repos;
+using System.Net.Http.Headers;
+
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+static async Task ProcessRepositoriesAsync(HttpClient client)
+{
+    string data = await client.GetStringAsync(
+        "https://localhost:7120/api/Teacher/count");
+
+    TeacherRepo teachers = new TeacherRepo(int.Parse(data));
+
+    Console.WriteLine(teachers.ToString());
+}
+
+await ProcessRepositoriesAsync(client);
