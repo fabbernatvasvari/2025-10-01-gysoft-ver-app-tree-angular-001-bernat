e48d06127b7036bea3360eaf58da1450e97ac974
jaydendeveloper <contact.jaydendev@gmail.com>
2025-10-01 09:31:43 +0200

    Data fetching refactor

--- src/net/Console/Program.cs
+++ src/net/Console/Program.cs
@@ -7,14 +7,16 @@ client.DefaultRequestHeaders.Accept.Add(
     new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
 client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
-static async Task ProcessRepositoriesAsync(HttpClient client)
+static async Task<string> GetTeacherData(HttpClient client)
 {
     string data = await client.GetStringAsync(
         "https://localhost:7120/api/Teacher/count");
 
-    TeacherRepo teachers = new TeacherRepo(int.Parse(data));
-
-    Console.WriteLine(teachers.ToString());
+    return data;
 }
 
-await ProcessRepositoriesAsync(client);
+string teacherData = await GetTeacherData(client);
+
+TeacherRepo teachers = new(int.Parse(teacherData));
+
+Console.WriteLine(teachers.ToString());
--- src/net/Console/Repos/TeacherRepo.cs
+++ src/net/Console/Repos/TeacherRepo.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyApp.Console.Models
+namespace MyApp.Console.Repos
 {
     internal class TeacherRepo
     {
