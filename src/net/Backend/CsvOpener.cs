3038c42c2bb741835023adb921645e475a82ecea
fabbernat <fabbernat@gmail.com>
2025-10-07 13:55:06 +0200

    add test data for sprint #2

---
+++ src/net/Backend/CsvOpener.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class CsvOpener
+{
+    public static void Main(string[] args)
+    {
+        using (var reader = new StreamReader(@"C:\VSCodeProjects\VasvariTanarErtekeloRendszer\Angular\gysoft-ver-app-tree-angular-001-bernat\test.csv"))
+        {
+            List<string> listA = new List<string>();
+            List<string> listB = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(';');
+
+                listA.Add(values[0]);
+                listB.Add(values[1]);
+            }
+        }
+    }
+}
+
