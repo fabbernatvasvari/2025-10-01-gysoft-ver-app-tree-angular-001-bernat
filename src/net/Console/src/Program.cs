7cbb2651c143f97c488bf54a7271e0f7de8eeebb
fabbernatvasvari <sze4fabber@vasvari.org>
2025-11-07 09:28:11 +0100

    Finished basic console program  with nice welcome message, no key input yet

---
+++ src/net/Console/src/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp;
+
+internal class Program
+{
+    public static void Log(string? message)
+    {
+        Console.WriteLine(message);
+    }
+    private static void Main(string[] args)
+    {
+        App app = new App();
+        app.Start();
+    }
+}
---
+++ src/net/Console/src/app/App.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp; 
+
+internal class App
+{
+
+    public void WaitSeconds(double seconds)
+    {
+        Thread.Sleep((int)(seconds * 1000));
+    }
+
+    internal void Start()
+    {
+        Program.Log($"Welcome back \"{Environment.UserName}\" to the VER App Cli! Type `help` to see valid commands");
+        while (true)
+        {
+            WaitSeconds(2);
+            Program.Log("The app is still running");
+        }
+    }
+}
