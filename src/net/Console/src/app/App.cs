abdc3e01c4bfef0430febd34695f2c415b898e59
fabbernatvasvari <sze4fabber@vasvari.org>
2025-11-07 09:37:28 +0100

    Implemented user key input scanning and processing exit

--- src/net/Console/src/app/App.cs
+++ src/net/Console/src/app/App.cs
@@ -1,4 +1,5 @@
-﻿namespace ConsoleApp; 
+﻿
+namespace ConsoleApp;
 
 internal class App
 {
@@ -11,10 +12,23 @@ internal class App
     internal void Start()
     {
         Program.Log($"Welcome back \"{Environment.UserName}\" to the VER App Cli! Type `help` to see valid commands");
+        Program.Log("type 'exit' / 'quit' to stop");
+
         while (true)
         {
             WaitSeconds(2);
             Program.Log("The app is still running");
+            Console.Write("> ");
+            string input = Console.ReadLine()?.Trim();
+
+            if (string.Equals(input, "exit", StringComparison.OrdinalIgnoreCase) ||
+                string.Equals(input, "quit", StringComparison.OrdinalIgnoreCase))
+            {
+                Program.Log("Exiting the program...");
+                break;
+            }
+
+            Program.Log($"You entered: {input}");
         }
     }
 }
