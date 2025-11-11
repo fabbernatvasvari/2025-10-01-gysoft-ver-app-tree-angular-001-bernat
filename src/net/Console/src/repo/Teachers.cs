fd271e530f05b9604529b52d55e04ae92625ba47
fabbernatvasvari <sze4fabber@vasvari.org>
2025-11-11 11:04:55 +0100

    Reader now properly reads in that gotten in parameter

---
+++ src/net/Console/src/repo/Teachers.cs
@@ -0,0 +1,14 @@
+﻿using MyApp.Console.src.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Console.src.repo
+{
+    internal class Teachers
+    {
+        public static List<string> AllTeachers = new List<Teacher>().Select(t => t.ToString()).ToList();
+    }
+}
--- src/net/Console/src/utils/Reader.cs
+++ src/net/Console/src/utils/Reader.cs
@@ -3,9 +3,23 @@ namespace MyApp.Console.src.utils
 {
     internal class Reader
     {
-        internal static void ReadCsv(string v)
+
+        // Returns the whole file as a single string
+        internal static string ReadCsvAsString(string path)
         {
-            throw new NotImplementedException();
+            if (!File.Exists(path))
+                throw new FileNotFoundException($"The file at {path} was not found.");
+
+            return File.ReadAllText(path);
+        }
+
+        // Returns the file as a list (array) of lines
+        internal static string[] ReadCsvAsList(string path)
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException($"The file at {path} was not found.");
+
+            return File.ReadAllLines(path);
         }
     }
 }
