532a5249874f788b523dee1f5344a78ba62e100c
fabbernatvasvari <sze4fabber@vasvari.org>
2025-11-11 11:20:53 +0100

    add reader to read the other file too

--- src/net/Console/src/utils/Reader.cs
+++ src/net/Console/src/utils/Reader.cs
@@ -1,8 +1,36 @@
 ﻿
+
+using ConsoleApp;
+
 namespace MyApp.Console.src.utils
 {
     internal class Reader
     {
+        static string teachersCsvPath = @"D:\MixedProjects\251107-gysoft-ver-app-bernat-egyeni\adatbazis\tesztadatok\tanarok.csv";
+        static string testDataCsvPath = @"D:\MixedProjects\251107-gysoft-ver-app-bernat-egyeni\adatbazis\tesztadatok\tesztadatok.csv";
+        internal static void ReadCsv(string command)
+        {
+            string csvContent = string.Empty;
+            string[] csvLines = Array.Empty<string>();
+
+            if ("teachers".Equals(command, StringComparison.OrdinalIgnoreCase))
+            {
+                csvContent = Reader.ReadCsvAsString(Reader.teachersCsvPath);
+                csvLines = Reader.ReadCsvAsList(Reader.teachersCsvPath);
+            } else if ("results".Equals(command, StringComparison.OrdinalIgnoreCase))
+            {
+                csvContent = Reader.ReadCsvAsString(Reader.testDataCsvPath);
+                csvLines = Reader.ReadCsvAsList(Reader.testDataCsvPath);
+            }
+
+            Program.Log(csvContent);
+
+            
+            foreach (var line in csvLines)
+            {
+                Program.Log(line);
+            }
+        }
 
         // Returns the whole file as a single string
         internal static string ReadCsvAsString(string path)
@@ -21,5 +49,6 @@ namespace MyApp.Console.src.utils
 
             return File.ReadAllLines(path);
         }
+
     }
 }
